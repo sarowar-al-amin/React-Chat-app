54e5fb5a1357ccb6dff52e084d3acf33da7b2024
Sarowar Al Amin <30774817+sarowar-al-amin@users.noreply.github.com>
2020-09-19 09:37:36 +0600

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,5 @@
 module.exports ={
-    mongoURI :'mongodb+srv://chat123:chat123@cluster0.qtc8s.mongodb.net/chat-app?retryWrites=true&w=majority'
+    mongoURI :'mongodb+srv://<user>:<password>@cluster0.qtc8s.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
+
+// create mongodb atlas cluster and put the link of that database with user name and respective password within the qoutetion marks.
